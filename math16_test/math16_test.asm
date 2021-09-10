6fd99c58f994f57c6ad184ebedf0e924cd1e3268
Neal Smith <nealvis@gmail.com>
2021-09-10 05:43:38 -0700

    cleanup

--- math16_test/math16_test.asm
+++ math16_test/math16_test.asm
@@ -13,22 +13,22 @@
 #import "../../nv_c64_util/nv_c64_util_macs_and_data.asm"
 
 *=$0800 "BASIC Start"
-.byte 0 // first byte should be 0
-        // location to put a 1 line basic program so we can just
-        // type run to execute the assembled program.
-        // will just call assembled program at correct location
-        //    10 SYS (4096)
+    .byte 0 // first byte should be 0
+    // location to put a 1 line basic program so we can just
+    // type run to execute the assembled program.
+    // will just call assembled program at correct location
+    //    10 SYS (4096)
 
-        // These bytes are a one line basic program that will 
-        // do a sys call to assembly language portion of
-        // of the program which will be at $1000 or 4096 decimal
-        // basic line is: 
-        // 10 SYS (4096)
-        .byte $0E, $08           // Forward address to next basic line
-        .byte $0A, $00           // this will be line 10 ($0A)
-        .byte $9E                // basic token for SYS
-        .byte $20, $28, $34, $30, $39, $36, $29 // ASCII for " (4096)"
-        .byte $00, $00, $00      // end of basic program (addr $080E from above)
+    // These bytes are a one line basic program that will 
+    // do a sys call to assembly language portion of
+    // of the program which will be at $1000 or 4096 decimal
+    // basic line is: 
+    // 10 SYS (4096)
+    .byte $0E, $08           // Forward address to next basic line
+    .byte $0A, $00           // this will be line 10 ($0A)
+    .byte $9E                // basic token for SYS
+    .byte $20, $28, $34, $30, $39, $36, $29 // ASCII for " (4096)"
+    .byte $00, $00, $00      // end of basic program (addr $080E from above)
 
 *=$0820 "Vars"
 
