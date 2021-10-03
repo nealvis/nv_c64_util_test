8455d62053d3db21b3bbc5a0716fcd41809a2204
Neal Smith <nealvis@gmail.com>
2021-10-02 19:26:29 -0700

    added tests for nv_mul16_y

--- math16_test/mul16_test.asm
+++ math16_test/mul16_test.asm
@@ -58,10 +58,14 @@ bad_neg_str: .text@" N\$00"
 
 title_str: .text @"MUL16\$00"          // null terminated string to print
                                         // via the BASIC routine
-title_mul16_x_1_str: .text @"TEST MUL16 x 1\$00"
-title_mul16_x_2_str: .text @"TEST MUL16 x 2\$00"
-title_mul16_x_3_str: .text @"TEST MUL16 x 3\$00"
+title_mul16_x_1_str: .text @"TEST MUL16 X 1\$00"
+title_mul16_y_1_str: .text @"TEST MUL16 Y 1\$00"
 
+title_mul16_x_2_str: .text @"TEST MUL16 X 2\$00"
+title_mul16_y_2_str: .text @"TEST MUL16 Y 2\$00"
+
+title_mul16_x_3_str: .text @"TEST MUL16 X 3\$00"
+title_mul16_y_3_str: .text @"TEST MUL16 Y 3\$00"
 
 title_mul16_immed_1_str: .text @"TEST MUL16 IMMED 1\$00"
 
@@ -105,7 +109,9 @@ op16_0008: .word $0008
 op16_0010: .word $0010
 op16_0020: .word $0020
 op16_0040: .word $0040
-
+op16_003F: .word $003F
+op16_007D: .word $007D
+op16_01DD: .word $01DD
 
 op8_7F: .byte $7F
 op8_FF: .byte $FF
@@ -114,6 +120,7 @@ op8_F0: .byte $F0
 op8_80: .byte $80  // -128
 op8_81: .byte $81  // -127
 op8_FE: .byte $FE
+op8_AA: .byte $AA
 
 op8_99: .byte $99
 op8_3F: .byte $3F  // 63
@@ -142,7 +149,11 @@ op8_40: .byte $40
     nv_screen_plot_cursor(row++, 33)
     nv_screen_print_str(title_str)
 
-    test_mul16_x_1(0)
+    test_mul16_1(true, 0)
+    test_mul16_2(true, 0)
+    test_mul16_1(false, 0)
+    test_mul16_2(false, 0)
+
 
     rts
 //////////////////////////////////////////////////////////////////////////////
@@ -150,131 +161,183 @@ op8_40: .byte $40
 
 //////////////////////////////////////////////////////////////////////////////
 //
-.macro test_mul16_x_1(init_row)
+.macro test_mul16_1(use_x_reg, init_row)
 {
     .var row = init_row
     
     //////////////////////////////////////////////////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    nv_screen_print_str(title_mul16_x_1_str)
+    .if (use_x_reg)
+    {
+        nv_screen_print_str(title_mul16_x_1_str)
+    }
+    else
+    {
+        nv_screen_print_str(title_mul16_y_1_str)
+    }
     //////////////////////////////////////////////////////////////////////////
     .eval row++
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_FFFF, op8_02, $FFFE, true)
+    print_mul16(use_x_reg, op16_0001, op8_01, $0001, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0001, op8_01, $0001, false)
+    print_mul16(use_x_reg, op16_0001, op8_02, $0002, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0001, op8_02, $0002, false)
-
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0002, op8_02, $0004, false)
+    print_mul16(use_x_reg, op16_0002, op8_02, $0004, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0007, op8_00, $0000, false)
+    print_mul16(use_x_reg, op16_0007, op8_00, $0000, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0000, op8_04, $0000, false)
+    print_mul16(use_x_reg, op16_0000, op8_04, $0000, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0000, op8_00, $0000, false)
+    print_mul16(use_x_reg, op16_0000, op8_00, $0000, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0000, op8_00, $0000, false)
+    print_mul16(use_x_reg, op16_0000, op8_00, $0000, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_0001, op8_03, $0003, false)
+    print_mul16(use_x_reg, op16_0001, op8_03, $0003, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_00FF, op8_FF, $FE01, false)
+    print_mul16(use_x_reg, op16_00FF, op8_FF, $FE01, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_FFFF, op8_01, $FFFF, false)
+    print_mul16(use_x_reg, op16_FFFF, op8_01, $FFFF, false)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_FFFF, op8_02, $FFFE, true)
+    print_mul16(use_x_reg, op16_FFFF, op8_02, $FFFE, true)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_FFFF, op8_03, $FFFD, true)
+    print_mul16(use_x_reg, op16_FFFF, op8_03, $FFFD, true)
 
     /////////////////////////////
     nv_screen_plot_cursor(row++, 0)
-    print_mul16_x(op16_FFFF, op8_FF, $FF01, true)
+    print_mul16(use_x_reg, op16_FFFF, op8_FF, $FF01, true)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_01DD, op8_AA, $3CC2, true)
 
     wait_and_clear_at_row(row)
 }
 
+
+//////////////////////////////////////////////////////////////////////////////
+//
+.macro test_mul16_2(use_x_reg, init_row)
+{
+    .var row = init_row
+    
+    //////////////////////////////////////////////////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    .if (use_x_reg)
+    {
+        nv_screen_print_str(title_mul16_x_2_str)
+    }
+    else
+    {
+        nv_screen_print_str(title_mul16_y_2_str)
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    .eval row++
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0001, op8_3F, $003F, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0002, op8_3F, $007E, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0003, op8_3F, $00BD, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0004, op8_3F, $00FC, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0005, op8_3F, $013B, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_0006, op8_3F, $017A, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_01, $003F, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_02, $007E, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_03, $00BD, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_04, $00FC, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_05, $013B, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_06, $017A, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_003F, op8_3F, $0F81, false)
+
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_007D, op8_02, $00FA, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_007D, op8_7D, $3D09, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_007D, op8_FF, $7C83, false)
+
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_00FF, op8_7D, $7C83, false)
+  
+    /////////////////////////////
+    nv_screen_plot_cursor(row++, 0)
+    print_mul16(use_x_reg, op16_00FF, op8_FF, $FE01, false)
+
+
+    wait_and_clear_at_row(row)
+
+}
 
 
 /*
-//////////////////////////////////////////////////////////////////////////////
-//
-.macro test_mul8_mem_mem_1(init_row)
-{
-    .var row = init_row
-    
-    //////////////////////////////////////////////////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    nv_screen_print_str(title_mul8_mem_mem_1_str)
-    //////////////////////////////////////////////////////////////////////////
-    .eval row++
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_01, op8_01, $0001)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_01, op8_02, $0002)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_02, op8_02, $0004)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_07, op8_00, $0000)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_00, op8_04, $0000)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_00, op8_00, $0000)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_01, op8_00, $0000)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_01, op8_03, $0003)
-
-    /////////////////////////////
-    nv_screen_plot_cursor(row++, 0)
-    print_mul8_mem_mem(op8_FF, op8_FF, $FE01)
-
-    wait_and_clear_at_row(row)
-
-}
-
 //////////////////////////////////////////////////////////////////////////////
 //
 .macro test_mul8_mem_mem_2(init_row)
@@ -491,7 +554,7 @@ WaitAnyKey:
 // addr1: address of word in memory thats op1 for multiplication
 // addr2: address of byte in memory thats op2 for multipliection
 // expected_result: is 16 bit immediate value that is the expected result
-.macro print_mul16_x(addr1, addr2, expected_result, expect_overflow_set)
+.macro print_mul16(use_x_reg, addr1, addr2, expected_result, expect_overflow_set)
 {
     // set passed to true until evidence of a fail below
     lda #1
@@ -508,8 +571,16 @@ WaitAnyKey:
     nv_screen_print_str(times_str)
 
     // do multiplication
-    ldx addr2
-    nv_mul16_x(addr1, result16, NV_PROCSTAT_OVERFLOW)
+    .if (use_x_reg)
+    {
+        ldx addr2
+        nv_mul16_x(addr1, result16, NV_PROCSTAT_OVERFLOW)
+    }
+    else 
+    {
+        ldy addr2
+        nv_mul16_y(addr1, result16, NV_PROCSTAT_OVERFLOW)
+    }
     php // save status register
     
     // if match expected result then skip setting passed to false
